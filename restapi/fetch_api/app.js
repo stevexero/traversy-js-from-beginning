7fde75164865bcc7c3bee63536e4f4f9eb8a509f
Steven Woodward <steveanthony999@gmail.com>
2020-05-11 12:50:02 -0700

    learned the fetch api

---
+++ restapi/fetch_api/app.js
@@ -0,0 +1,81 @@
+document.getElementById('button1').addEventListener('click', getText);
+document.getElementById('button2').addEventListener('click', getJson);
+document.getElementById('button3').addEventListener('click', getExternal);
+
+// Get Local Text File Data
+function getText() {
+  fetch('test.txt')
+    .then(function(res) {
+      return res.text();
+    })
+    .then(function(data) {
+      console.log(data);
+      document.getElementById('output').innerHTML = data;
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
+// Get Local JSON Data
+function getJson() {
+  fetch('posts.json')
+    .then(function(res) {
+      return res.json();
+    })
+    .then(function(data) {
+      console.log(data);
+      let output = '';
+      data.forEach(function(post) {
+        output += `<li>${post.title}</li>`;
+      });
+      document.getElementById('output').innerHTML = output;
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
+// Get from external API
+function getExternal() {
+  fetch('https://api.github.com/users')
+    .then(function(res) {
+      return res.json();
+    })
+    .then(function(data) {
+      console.log(data);
+      let output = '';
+      data.forEach(function(user) {
+        output += `<li>${user.repos_url}</li>`;
+      });
+      document.getElementById('output').innerHTML = output;
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
+// Get from external API with error handling
+function getExternal() {
+  fetch('https://api.github.com/users')
+    .then(res => res.json())
+    .then(res => {
+      if (!res.ok) {
+        throw new Error(res.error);
+      }
+      return res;
+    })
+    .catch(err => console.log(err));
+}
+
+// separate function for error handling
+function handleErrors(res) {
+  if (!res.ok) throw new Error(res.error);
+  return res;
+}
+
+fetch('https://api.github.com/users')
+  .then(res => res.json())
+  .then(handleErrors)
+  .then(res => console.log(res.data))
+  .catch(err => console.log(err));
