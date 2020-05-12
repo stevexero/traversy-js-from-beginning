a66a2582f40828ddad6c5725cdb8fafc09b63daa
Steven Woodward <steveanthony999@gmail.com>
2020-05-12 14:47:27 -0700

    display weather

--- api_projects/weather_app/app.js
+++ api_projects/weather_app/app.js
@@ -1,6 +1,9 @@
 // Initialize weather object
 const weather = new Weather('las vegas');
 
+// Init UI
+const ui = new UI();
+
 // Get weather on DOM Load
 document.addEventListener('DOMContentLoaded', getWeather);
 
@@ -10,6 +13,7 @@ function getWeather() {
   weather
     .getWeather()
     .then(results => {
+      ui.paint(results);
       console.log(results);
     })
     .catch(err => console.log(err));
--- api_projects/weather_app/ui.js
+++ api_projects/weather_app/ui.js
@@ -1,1 +1,23 @@
-class UI {}
+class UI {
+  constructor() {
+    this.location = document.getElementById('w-location');
+    this.desc = document.getElementById('w-desc');
+    this.string = document.getElementById('w-string');
+    this.details = document.getElementById('w-details');
+    // this.icon = document.getElementById('w-icon');
+    this.humidity = document.getElementById('w-humidity');
+    this.feelsLike = document.getElementById('w-feels-like');
+    // this.dewpoint = document.getElementById('w-dewpoint');
+    this.wind = document.getElementById('w-wind');
+  }
+
+  paint(weather) {
+    this.location.textContent = weather.name;
+    this.desc.textContent = weather.weather[0].description;
+    this.string.textContent = Math.round((weather.main.temp * 9) / 5 + 32);
+    // this.icon.setAttribute('src', weather.weather[0].icon);
+    this.humidity.textContent = `Relative Humidity: ${weather.main.humidity}`;
+    this.feelsLike.textContent = `Feels Like: ${weather.main.feels_like}`;
+    this.wind.textContent = `Wind: ${weather.wind.speed}`;
+  }
+}
