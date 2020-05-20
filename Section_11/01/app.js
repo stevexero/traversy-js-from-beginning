f2ef1b62e0df93120a1b8bd1d5f703fc0c177bdd
Steven Woodward <steveanthony999@gmail.com>
2020-05-20 12:18:21 -0700

    learned module & revealing module pattern

---
+++ Section_11/01/app.js
@@ -0,0 +1,58 @@
+// Basic Structure
+
+// iife immediately invokes function expression
+// (function() {
+//     // Declare private vars and functions
+
+//     return {
+//         // Declare public vars and functions
+//     }
+// })()
+
+// STANDARD MODULE PATTERN
+// const UICtrl = (function() {
+//   let text = 'Hello World';
+
+//   const changeText = function() {
+//     const element = document.querySelector('h1');
+//     element.textContent = text;
+//   };
+
+//   return {
+//     callChangeText: function() {
+//       changeText();
+//       console.log(text);
+//     }
+//   };
+// })();
+
+// UICtrl.callChangeText();
+
+// console.log(UICtrl);
+
+//
+// REVEALING MODULE PATTERN
+const ItemCtrl = (function() {
+  let _data = [];
+
+  function add(item) {
+    _data.push(item);
+    console.log('Item Added');
+  }
+
+  function get(id) {
+    return _data.find(item => {
+      return item.id === id;
+    });
+  }
+
+  return {
+    add: add,
+    get: get
+  };
+})();
+
+ItemCtrl.add({ id: 1, name: 'John' });
+ItemCtrl.add({ id: 2, name: 'Mark' });
+console.log(ItemCtrl.get(1));
+console.log(ItemCtrl.get(2));
