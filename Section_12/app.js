5d0a5ee3c3ddde101987786759f6753fd5489180
Steven Woodward <steveanthony999@gmail.com>
2020-05-20 21:46:10 -0700

    learned how to get and populate items

--- Section_12/app.js
+++ Section_12/app.js
@@ -22,6 +22,9 @@ const ItemCtrl = (function() {
 
   //   Public Methods
   return {
+    getItems: function() {
+      return data.items;
+    },
     logData: function() {
       return data;
     }
@@ -30,8 +33,27 @@ const ItemCtrl = (function() {
 
 // UI Controller
 const UICtrl = (function() {
+  const UISelectors = {
+    itemList: '#item-list'
+  };
+
   // Public Methods
-  return {};
+  return {
+    populateItemList: function(items) {
+      let html = '';
+
+      items.forEach(function(item) {
+        html += `
+            <li class="collection-item" id="item-${item.id}">
+            <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+            <a href="#" class="secondary-content"><i class="edit-item fa fa-pencil"></i></a>
+            </li>`;
+      });
+
+      //   Insert list items
+      document.querySelector(UISelectors.itemList).innerHTML = html;
+    }
+  };
 })();
 
 // App Controller
@@ -39,7 +61,11 @@ const App = (function(ItemCtrl, UICtrl) {
   // Public methods
   return {
     init: function() {
-      console.log('Initializing App...');
+      //   Fetch Items from data structure
+      const items = ItemCtrl.getItems();
+
+      //   Populate list with items
+      UICtrl.populateItemList(items);
     }
   };
 })(ItemCtrl, UICtrl);
