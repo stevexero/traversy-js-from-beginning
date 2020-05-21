d798d17f21dac0a26d3b1227ed56e5ba22bb467a
Steven Woodward <steveanthony999@gmail.com>
2020-05-21 10:58:27 -0700

    learned about the edit state

--- Section_12/app.js
+++ Section_12/app.js
@@ -50,6 +50,27 @@ const ItemCtrl = (function() {
       return newItem;
     },
 
+    getItemById: function(id) {
+      let found = null;
+
+      // Loop through items
+      data.items.forEach(function(item) {
+        if (item.id === id) {
+          found = item;
+        }
+      });
+
+      return found;
+    },
+
+    setCurrentItem: function(item) {
+      data.currentItem = item;
+    },
+
+    getCurrentItem: function() {
+      return data.currentItem;
+    },
+
     getTotalCalories: function() {
       let total = 0;
 
@@ -80,6 +101,9 @@ const UICtrl = (function() {
   const UISelectors = {
     itemList: '#item-list',
     addBtn: '.add-btn',
+    updateBtn: '.update-btn',
+    deleteBtn: '.delete-btn',
+    backBtn: '.back-btn',
     itemNameInput: '#item-name',
     itemCaloriesInput: '#item-calories',
     totalCalories: '.total-calories'
@@ -131,6 +155,17 @@ const UICtrl = (function() {
       document.querySelector(UISelectors.itemCaloriesInput).value = '';
     },
 
+    addItemToForm: function() {
+      document.querySelector(
+        UISelectors.itemNameInput
+      ).value = ItemCtrl.getCurrentItem().name;
+      document.querySelector(
+        UISelectors.itemCaloriesInput
+      ).value = ItemCtrl.getCurrentItem().calories;
+
+      UICtrl.showEditState();
+    },
+
     hideList: function() {
       document.querySelector(UISelectors.itemList).style.display = 'none';
     },
@@ -139,6 +174,21 @@ const UICtrl = (function() {
       document.querySelector(
         UISelectors.totalCalories
       ).textContent = totalCalories;
+    },
+
+    clearEditState: function() {
+      UICtrl.clearInput();
+      document.querySelector(UISelectors.updateBtn).style.display = 'none';
+      document.querySelector(UISelectors.deleteBtn).style.display = 'none';
+      document.querySelector(UISelectors.backBtn).style.display = 'none';
+      document.querySelector(UISelectors.addBtn).style.display = 'inline';
+    },
+
+    showEditState: function() {
+      document.querySelector(UISelectors.updateBtn).style.display = 'inline';
+      document.querySelector(UISelectors.deleteBtn).style.display = 'inline';
+      document.querySelector(UISelectors.backBtn).style.display = 'inline';
+      document.querySelector(UISelectors.addBtn).style.display = 'none';
     },
 
     getSelectors: function() {
@@ -162,6 +212,11 @@ const App = (function(ItemCtrl, UICtrl) {
     document
       .querySelector(UISelectors.addBtn)
       .addEventListener('click', itemAddSubmit);
+
+    //   Edit Icon Click Event
+    document
+      .querySelector(UISelectors.itemList)
+      .addEventListener('click', itemUpdateSubmit);
   };
 
   //   Add item submit
@@ -190,9 +245,37 @@ const App = (function(ItemCtrl, UICtrl) {
     e.preventDefault();
   };
 
+  //   Update item submit
+  const itemUpdateSubmit = function(e) {
+    if (e.target.classList.contains('edit-item')) {
+      //   Get list item id (item-o, item-1)
+      const listId = e.target.parentNode.parentNode.id;
+
+      //   Break into an array
+      const listIdArr = listId.split('-');
+
+      //   Get the actual id
+      const id = parseInt(listIdArr[1]);
+
+      //   Get item
+      const itemToEdit = ItemCtrl.getItemById(id);
+
+      //   Set Current Item
+      ItemCtrl.setCurrentItem(itemToEdit);
+
+      //   Add item to form
+      UICtrl.addItemToForm();
+    }
+
+    e.preventDefault();
+  };
+
   // Public methods
   return {
     init: function() {
+      // Clear edit state / set initial state
+      UICtrl.clearEditState();
+
       //   Fetch Items from data structure
       const items = ItemCtrl.getItems();
 
@@ -218,6 +301,7 @@ const App = (function(ItemCtrl, UICtrl) {
 
 //
 //
-//
+// ********************************************************************
 // Initialize App
+// ********************************************************************
 App.init();
